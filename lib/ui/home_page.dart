afd55c0354067e525bfb29a4db44dc1274f50ac0
Erick <j.erickleandro@gmail.com>
2019-07-22 00:18:35 -0300

    Home page - conection with api - search field

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _search;
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    if (_search == null) {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=rFU4yWksE0SSVQpeqMCB8OvC9YxBe8KK&limit=25&rating=G");
+    } else {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=rFU4yWksE0SSVQpeqMCB8OvC9YxBe8KK&q=$_search&limit=25&offset=$_offset&rating=G&lang=en");
+    }
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map) {
+      print(map);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Image.network(
+            "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.deepPurpleAccent,
+      body: Column(
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Pesquise aqui!",
+                    labelStyle: TextStyle(color: Colors.white),
+                    border: OutlineInputBorder()),
+                style: TextStyle(color: Colors.white, fontSize: 18.0),
+                textAlign: TextAlign.center,
+              ))
+        ],
+      ),
+    );
+  }
+}
