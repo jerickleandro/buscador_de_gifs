a0045ab23d7abcf2b531d05ace8d2cb6f84c78fe
Erick <j.erickleandro@gmail.com>
2019-08-09 17:20:51 -0300

    boton share en long press share implemented

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:buscador_gifs/ui/gif_page.dart';
+import 'package:share/share.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -121,6 +123,15 @@ class _HomePageState extends State<HomePage> {
             height: 300.0,
             fit: BoxFit.cover,
           ),
+          onTap: (){
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => GifPage(snapshot.data["data"][index]))
+                );
+              },
+          onLongPress: (){
+            Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
+            
+          },
           );
         }
         else{
